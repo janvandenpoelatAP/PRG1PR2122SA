e102cf80345722ac383051ef12660147f4a12d7a
janvandenpoel <jan.vandenpoel@vdpc.be>
2022-03-13 14:34:05 +0100

    Opdracht 1

---
+++ SchoolAdmin/Student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchoolAdmin
+{
+    public class Student
+    {
+        public static uint Sudententeller;
+        public string Naam;
+        public DateTime GeboorteDatum;
+        public uint Sudentennummer;
+        public string[] Cursussen;
+        
+    }
+}
