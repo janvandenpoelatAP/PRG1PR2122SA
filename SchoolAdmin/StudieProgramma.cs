e773be12009df6eb6c90c1bb4ea143e55a1b30ef
janvandenpoel <jan.vandenpoel@vdpc.be>
2022-03-20 15:21:34 +0100

    Opdracht 16

--- SchoolAdmin/StudieProgramma.cs
+++ SchoolAdmin/StudieProgramma.cs
@@ -51,6 +51,7 @@ namespace SchoolAdmin
         }
         public static void DemonstreerStudieProgrmma()
         {
+            /* Stap 1
 	        Cursus communicatie = new Cursus("Communicatie");
             Cursus programmeren = new Cursus("Programmeren");
             Cursus databanken = new Cursus("Databanken", new Student[7], 5);
@@ -64,6 +65,23 @@ namespace SchoolAdmin
             snbProgramma.cursussen[2] = null;
             programmerenProgramma.ToonOverzicht();
             snbProgramma.ToonOverzicht();
+            */
+            Cursus communicatie = new Cursus("Communicatie");
+	        Cursus programmeren = new Cursus("Programmeren");
+	        Cursus databanken = new Cursus("Databanken", new Student[7], 5);
+	        Cursus[] cursussenProgrammeren = { communicatie, programmeren, databanken };
+	        Cursus[] cursussenSnb = { communicatie, programmeren, databanken };
+	        StudieProgramma programmerenProgramma = new StudieProgramma("Programmeren");
+	        StudieProgramma snbProgramma = new StudieProgramma("Systeem- en netwerkbeheer");
+	        programmerenProgramma.cursussen = cursussenProgrammeren;
+	        snbProgramma.cursussen = cursussenSnb;
+            // later wordt Databanken geschrapt uit het programma SNB
+	        // voor SNB wordt bovendien Programmeren hernoemd naar Scripting
+            Cursus scripting = new Cursus("Scripting");
+	        snbProgramma.cursussen[2] = null;
+	        snbProgramma.cursussen[1]= scripting;;
+	        programmerenProgramma.ToonOverzicht();
+	        snbProgramma.ToonOverzicht();
         }
     }
 }
