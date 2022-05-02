b03e66ae98cfce2e87817bf4ad944118c1bea237
janvandenpoelatAP <jan.vandenpoel@ap.be>
2022-05-02 12:28:09 +0200

    Opdracht 25

--- SchoolAdmin/Program.cs
+++ SchoolAdmin/Program.cs
@@ -7,7 +7,7 @@ namespace SchoolAdmin
         static void Main(string[] args)
         {
             int keuze = 0;
-            Console.WriteLine($"Wat wil je demonstreren?\n\t1. Studenten\n\t2. Cursussen\n\t3. Student Uit tekst\n\t4. StudieProgramma\n\t5. Administratief Personeel\n");
+            Console.WriteLine($"Wat wil je demonstreren?\n\t1. Studenten\n\t2. Cursussen\n\t3. Student Uit tekst\n\t4. StudieProgramma\n\t5. Administratief Personeel\n\t6. Lector\n");
             keuze = Convert.ToInt32(Console.ReadLine());
             if (keuze == 1)
             {
@@ -28,6 +28,10 @@ namespace SchoolAdmin
             else if (keuze == 5)
             {
                 AdministratiefPersoneel.DemonstreerAdministratiefPersoneel();
+            }
+            else if (keuze == 6)
+            {
+                Lector.DemonstreerLectoren();
             }
         }
     }
